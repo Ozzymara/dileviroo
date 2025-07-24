d2ef90cc0d96255122633b005cacf23883c3286c
ozzymara <maraviaus@yahoo.com>
2025-07-24 10:41:37 +0100

    base.html created

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.contrib import messages
+from cafe.models import *
+from django.core.files.storage import FileSystemStorage
+from datetime import date, datetime, timedelta
+import json
+import ast
 
-# Create your views here.
+from itertools import groupby
+from django.db.models import Sum
+from .models import Order, User
+
+User = get_user_model()
