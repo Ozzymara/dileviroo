15dd0e503bddf9babfee5de00fd0102bfe821226
ozzymara <maraviaus@yahoo.com>
2025-08-04 15:05:58 +0100

    removed test text from menu template

--- dileviroo/settings.py
+++ dileviroo/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import dj_database_url
 
 if os.path.isfile('env2.py'):
-    import env
+    import env2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
